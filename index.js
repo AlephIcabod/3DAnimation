b8e9a825b9af8e95eaf106b80a61a65c1ab64cd9
alephIcabod <angel_507@live.com.mx>
2017-07-16 23:55:19 -0500

    animation corrections

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 (function(d){    
-    d.getElementById('menu').addEventListener('click',(e)=>{
-        d.getElementById('menu').classList.toggle('cerrado');
+    
+    d.getElementById('menu_close').addEventListener('click',e=>{
+        var x=document.getElementsByClassName('menu_column');    
+        x[2].classList.add('flip');
+        setTimeout(function(){
+            x[0].classList.add('flip');
+        },500)
+    });
+
+    d.getElementById('menu_open').addEventListener('click',e=>{
+        var x=document.getElementsByClassName('menu_column');    
+        x[0].classList.remove('flip');
+        setTimeout(function(){
+            x[2].classList.remove('flip');
+        },500)
     });
 })(document);
