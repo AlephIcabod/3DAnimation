d6dde4e366bc90988b9709f28c349d8c0d8acd94
alephIcabod <angel_507@live.com.mx>
2017-07-26 20:22:58 -0500

    second  animation

--- multilevelMenu/index.js
+++ multilevelMenu/index.js
@@ -1,45 +1,33 @@
-const toggleMenu=(id,men,navID)=>{
+const toggleMenu=(id,men,animation='fadeIn')=>{
     let toggle=document.getElementById(id);
-    let menu=document.getElementById(men);
-    let nav=document.getElementById(navID);
+    let menu=document.getElementById(men);    
     toggle.addEventListener('click',e=>{        
-        menu.querySelectorAll('.show').forEach(i=>i.classList.remove('show'));
-        menu.classList.toggle('show');
+        menu.querySelectorAll(`.show-${animation}`).forEach(i=>i.classList.remove(`show-${animation}`));                
+        if(!menu.classList.contains(`show-${animation}`))
+            menu.classList.add(`show-${animation}`);
+        else
+            menu.classList.remove(`show-${animation}`);
     });
     if(menu.querySelector('.submenu'))
-        viewSubmenu(menu,nav)
+        viewSubmenu(menu,animation)
 }
 
-const viewSubmenu=(men,nav)=>{
+const viewSubmenu=(men,animation)=>{
     let subs=men.querySelectorAll('.submenu');    
     subs.forEach(i=>{
         i.parentElement.querySelector('.menu_link').addEventListener('click',e=>{
-             let v=document.querySelectorAll('.show');
-              v.forEach(j=>{j.classList.remove('show');
-                    fadeOut(j);
-                });              
-              i.classList.add('show');
-              fadeIn(i);              
+             let v=document.querySelectorAll(`.show-${animation}`);             
+              v.forEach(j=>j.classList.remove(`show-${animation}`));
+              let x=i.parentElement.parentElement;
+              x.style.height='0';
+              i.classList.add(`show-${animation}`);
         });
         i.querySelector('.back').addEventListener('click',e=>{
-            i.classList.remove('show');
-            fadeOut(i);
-            i.parentElement.parentElement.classList.add('show');
-            fadeIn(i.parentElement.parentElement)
+            i.classList.remove(`show-${animation}`);            
+            i.parentElement.parentElement.classList.add(`show-${animation}`);            
         })
     });
 }
 
 
-const fadeIn=(cont)=>{
-    const childs=cont.children;
-    for(let i of childs)
-    i.classList.add('fadeIn')
-}
-
-const fadeOut=(cont)=>{
-    const childs=cont.children;
-    for(let i of childs)
-    i.classList.remove('fadeIn')
-}
-toggleMenu('menu-toggle','main-menu','main-nav');
+toggleMenu('menu-toggle','main-menu','explode');
